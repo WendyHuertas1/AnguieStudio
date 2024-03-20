ddb6c5bf3b865e41562d3b5e81810b01c538373c
APRENDIZ <APRENDIZ@BOGCPCDMF10406.FormacionSena.RED>
2024-03-20 11:02:01 -0500

    Cambios en platillas

--- Bases de datos/login.sql
+++ Bases de datos/login.sql
@@ -121,5 +121,5 @@ INSERT INTO usuarios (id, correo, password, id_rol, nombre, apellido, telefono) 
 (2, 'dilanjimenez208@gmail.com', '123', 2, 'Dilan', 'Jimenez', 234234),
 (3, 'wendy2408@outlook.es', '1234', 3, 'Lorena', 'Hernandez', 423423),
 (5, 'alejandro@gmail.com', '456', 3, 'Alejandro', 'Martinez', 2147483647),
-(6, 'alejandro@gmail.com', '456', 3, 'Alejandro', 'Martinez', 2147483647);
+(6, 'alejandro@gmail.com', '456', 3, 'Alejandro', 'Martinez', 2147483647),
 (7, 'dilanjimenez200@gmail.com', '1234567', 1, 'Dilan', 'Jimenez', 123124);
